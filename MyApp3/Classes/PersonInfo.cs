95e1a2f3017af78fed7aef984794bbc5f9e62aac
Mihailova Anastasiya <avmihailova7392@gmail.com>
2026-09-22 15:12:12 +0500

    Разработанный класс. Фамилия.

---
+++ MyApp3/Classes/PersonInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp3.Classes
+{
+    public class PersonInfo
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+        public int Level { get; set; }
+        public int Glasses { get; set; }
+        public int Money { get; set; }
+        public float Damage { get; set; }
+    
+    public PersonInfo(string Name, int Health, int Armor, int Level, int Glasses, int Money, float Damage) {
+            this.Name = Name;
+            this.Health = Health;
+            this.Armor = Armor;
+            this.Level = Level;
+            this.Glasses = Glasses;
+            this.Money = Money;
+            this.Damage = Damage;
+        }
+    } 
+}
